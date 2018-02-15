6ca8bec320fc8c0fc635bf4a7d88bf1e9ce40b67
Aric Alves <aric.alves@yahoo.com>
2018-02-14 19:40:51 -0800

    Add dependencies and successfully write packages to file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 'use strict'
 
+const asyncEach = require('each-async');
+const download = require('npm-registry-download'); // lol npm
+
 const getPackageNames = require('./get');
 
 function downloadPackages (count, callback) {
-
+  getPackageNames(count)
+    .then(listOfPackages => {
+      asyncEach(listOfPackages, function(title) {
+        download(title, { dir: `${__dirname}/packages/${title}` })
+      });
+    })
+    .catch(err => console.log(err));
 }
 
 module.exports = downloadPackages
