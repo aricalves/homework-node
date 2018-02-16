fe8ddd8a2c92025f9958f098d1da8055f6ae4eee
Aric Alves <aric.alves@yahoo.com>
2018-02-16 15:53:45 -0800

    Add async module, properly return callback

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict'
 
-const asyncEach = require('each-async');
-const download = require('npm-registry-download');
+const { each } = require('async');
+const { download } = require('nrd');
 
 const getPackageNames = require('./get');
 
-function downloadPackages (count, callback) {
+function downloadPackages(count, callback) {
   getPackageNames(count)
-    .then(listOfPackages => {
-      asyncEach(listOfPackages, function(title) {
-        download(title, { dir: `${__dirname}/packages/${title}` });
-      });
+    .then(listOfPackages => { 
+      each(listOfPackages, (title, cb) => {
+          download(title, { dir: `${__dirname}/packages/${title}` })
+            .then(() => cb())
+        },
+        function (err) {
+          if (err) { throw err; }
+          return callback();
+        })
     })
-    .then(() => callback())
     .catch(err => callback(err));
 };
 
